9d4f4068b8720fdb7fba3a897352e7896f378381
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-10-25 17:54:01 +0900

    ✨ 메인 라우트에 AuthRoute 추가

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 8000;
 
+//Router
+const authRoutes = require("./routes/authRoute");
+
 // 데이터베이스 연결
 mongoose
   .connect(process.env.MONGO_URI)
@@ -17,7 +20,16 @@ mongoose
   .catch((err) => console.error("MongoDB 연결 실패:", err));
 
 // 미들웨어 설정
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(
+  cors({
+    origin: [
+      process.env.CORS_ORIGIN,
+      "http://localhost:5173",
+      "http://localhost:8000",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json()); // JSON 파싱
 app.use(cookieParser()); // 쿠키 파싱
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs)); // Swagger UI를 /api-docs 경로에 라우팅
@@ -27,12 +39,13 @@ app.get("/", (req, res) => {
   res.send("Express 서버가 실행 중입니다.");
 });
 
+// 추가적인 라우터를 추가하려면, routes 폴더에 파일을 추가하고, 이곳에서 불러오면 됨
+app.use("/api/auth", authRoutes);
+
 // 존재하지 않는 라우트에 대한 처리
 app.use("*", (req, res) => {
   res.status(404).json({ message: "요청하신 페이지를 찾을 수 없습니다." });
 });
-
-// 추가적인 라우터를 추가하려면, routes 폴더에 파일을 추가하고, 이곳에서 불러오면 됨
 
 // 서버 시작
 app.listen(port, () => {
