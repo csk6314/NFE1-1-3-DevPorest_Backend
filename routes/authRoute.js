efe79dc78045432f678c93083766ced717437e25
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-10-25 17:51:33 +0900

    ✨ AuthRoute 구현

---
+++ routes/authRoute.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+const express = require("express");
+const authController = require("../controllers/authController");
+const router = express.Router();
+
+/**
+ * @openapi
+ * '/api/auth/github':
+ *  get:
+ *     tags:
+ *     - Auth
+ *     summary: Github OAuth 로그인 API
+ *     responses:
+ *      200:
+ *        description: Successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
+router.get("/github", authController.getGithubRedirectURL);
+
+/**
+ * @openapi
+ * '/api/auth/github/callback':
+ *  get:
+ *     tags:
+ *     - Auth
+ *     summary: Github OAuth Callback API
+ *     parameters:
+ *      - name: id
+ *        in: query
+ *        description: Github Authorization Code
+ *        schema:
+ *          type: string
+ *     responses:
+ *      200:
+ *        description: Successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
+router.get("/github/callback", authController.getGithubCallback);
+
+/**
+ * @openapi
+ * '/api/auth/logout':
+ *  get:
+ *     tags:
+ *     - Auth
+ *     summary: Logout API
+ *     responses:
+ *      200:
+ *        description: Successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
+router.get("/logout", authController.logout);
+
+module.exports = router;
