5195169b69dd45dbace2d04511306058f62ed57b
dalsu0222 <dalsu0222@sju.ac.kr>
2024-10-25 23:54:41 +0900

    ♻️ auth 라우터 설정 변경

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 // 라우트 파일들을 import
 const portfolioRoutes = require("./portfolioRoutes");
+const authRoutes = require("./authRoute");
 
 // API 라우트 설정
 router.use("/portfolios", portfolioRoutes);
+router.use("/auth", authRoutes);
 
 module.exports = router;
