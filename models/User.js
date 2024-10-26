dbcc9f248e1df4130d936feb33039a4a5fa6a253
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-10-26 20:10:13 +0900

    🔥 User Schema createdAt 필드 required 삭제

--- models/User.js
+++ models/User.js
@@ -31,7 +31,6 @@ const userSchema = new Schema({
   links: [{ type: String }],
   createdAt: {
     type: Date,
-    required: true,
     default: Date.now, // UTC 기준으로 저장, 조회 시 한국 시간으로 변환 필요
   },
   techStack: [{ type: Schema.Types.ObjectId, ref: "TechStack" }],
