11aedabd2fc2b47cd5aa925ce41842a299a39f6e
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-10-27 03:48:27 +0900

    ✨ techstack route 생성 및 연결

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 // 라우트 파일들을 import
 const portfolioRoutes = require("./portfolioRoutes");
 const authRoutes = require("./authRoute");
+const techStackRoutes = require("./techStackRoute");
 
 // API 라우트 설정
 router.use("/portfolios", portfolioRoutes);
 router.use("/auth", authRoutes);
+router.use("/techstacks", techStackRoutes);
 
 module.exports = router;
