4ff6cc1c11dbc5d6c9b9fbb497d25ac0bc3efc49
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-10-28 17:26:22 +0900

    ✨ JobGroup Schema 작성

---
+++ models/JobGroup.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const jobGroupSchema = new Schema({
+  job: {
+    type: String,
+    required: true,
+  },
+});
+
+const JobGroup = mongoose.model("JobGroup", jobGroupSchema);
+module.exports = JobGroup;
