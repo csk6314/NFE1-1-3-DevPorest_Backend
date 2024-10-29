63c83649a2e672f76e46181da69c68eb90876b93
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-10-29 09:54:35 +0900

    ♻️ 불필요한 import 삭제

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-//schema
-const User = require("../models/User");
-
 //controller
 const userController = require("../controllers/userController");
 
