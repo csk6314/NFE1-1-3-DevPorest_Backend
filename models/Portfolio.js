d407da7db453bd88c55e9775868b65346181a4aa
dalsu0222 <dalsu0222@sju.ac.kr>
2024-10-29 12:10:29 +0900

    🗃️ ✨ Comment 스키마 작성 및 timestamp 제거

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -1,59 +1,54 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const portfolioSchema = new Schema(
-  {
-    // _id : 자동생성됨
-    title: {
-      type: String,
-      required: true,
-    },
-    contents: {
-      type: String,
-      required: true,
-    },
-    view: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    images: [{ type: String, required: true }], // 각 배열요소의 상세 컨트롤 가능
-    tags: [
-      {
-        type: String,
-        required: false,
-      },
-    ],
-    createdAt: {
-      type: Date,
-      required: true,
-      default: Date.now, // UTC 기준으로 저장, 조회 시 한국 시간으로 변환 필요
-    },
-    techStack: [
-      {
-        type: String,
-        required: true,
-      },
-    ],
-    jobGroup: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: "JobGroup", // JobGroup 모델을 참조
-    },
-    thumbnailImage: {
-      type: String,
-      default: null,
-    },
-    userID: {
-      type: String,
-      required: true,
-      ref: "User", // User 모델을 참조
-    },
+const portfolioSchema = new Schema({
+  // _id : 자동생성됨
+  title: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  contents: {
+    type: String,
+    required: true,
+  },
+  view: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  images: [{ type: String, required: true }], // 각 배열요소의 상세 컨트롤 가능
+  tags: [
+    {
+      type: String,
+      required: false,
+    },
+  ],
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now, // UTC 기준으로 저장, 조회 시 한국 시간으로 변환 필요
+  },
+  techStack: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  jobGroup: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "JobGroup", // JobGroup 모델을 참조
+  },
+  thumbnailImage: {
+    type: String,
+    default: null,
+  },
+  userID: {
+    type: String,
+    required: true,
+    ref: "User", // User 모델을 참조
+  },
+});
 
 // 1은 오름차순, -1은 내림차순 정렬을 의미합니다
 portfolioSchema.index({ userID: 1 }); // 특정 사용자의 모든 포트폴리오 검색 시 성능 향상
