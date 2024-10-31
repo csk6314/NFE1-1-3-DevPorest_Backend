1741b0fe0f09898e2f7ac628e240d1d04e01f1c8
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-10-31 14:50:03 +0900

    ♻️ 유저 API pipeline 별도 파일로 분리

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const mongoose = require("mongoose");
+const { userProfilePipeline } = require("../pipeline/userPipeline");
 
 const getUserInfo = async (req, res) => {
   const { userid } = req.params;
@@ -7,91 +8,7 @@ const getUserInfo = async (req, res) => {
   try {
     const user = await User.aggregate([
       { $match: { userID: userid } },
-      {
-        $lookup: {
-          // 유저의 portfolio와 join
-          from: "portfolios",
-          localField: "userID",
-          foreignField: "userID",
-          as: "user_portfolios",
-          pipeline: [
-            {
-              $project: {
-                _id: 1,
-              },
-            },
-          ],
-        },
-      },
-      {
-        $lookup: {
-          // 각 유저 포트폴리오와 like join
-          from: "likes",
-          localField: "user_portfolios._id",
-          foreignField: "portfolioID",
-          as: "post_likes",
-          pipeline: [
-            {
-              $project: {
-                _id: 1,
-              },
-            },
-          ],
-        },
-      },
-      {
-        $addFields: {
-          totalLikes: { $size: "$post_likes" },
-        },
-      },
-      {
-        $project: {
-          user_portfolios: 0,
-          post_likes: 0,
-          __v: 0,
-          _id: 0,
-        },
-      },
-      {
-        $lookup: {
-          from: "techstacks",
-          localField: "techStack",
-          foreignField: "skill",
-          as: "techStack",
-          pipeline: [
-            {
-              $project: {
-                __v: 0,
-                _id: 0,
-              },
-            },
-          ],
-        },
-      },
-      {
-        $lookup: {
-          from: "jobgroups",
-          localField: "jobGroup",
-          foreignField: "_id",
-          as: "jobGroup",
-          pipeline: [
-            {
-              $project: {
-                __v: 0,
-                _id: 0,
-              },
-            },
-          ],
-        },
-      },
-      {
-        $unwind: "$jobGroup",
-      },
-      {
-        $addFields: {
-          jobGroup: "$jobGroup.job",
-        },
-      },
+      ...userProfilePipeline,
     ]);
 
     if (user.length < 1) {
@@ -192,105 +109,7 @@ const modifyUserProfile = async (req, res) => {
 const getPopularUserProfile = async (req, res) => {
   try {
     const user = await User.aggregate([
-      {
-        $lookup: {
-          // 유저의 portfolio와 join
-          from: "portfolios",
-          localField: "userID",
-          foreignField: "userID",
-          as: "user_portfolios",
-          pipeline: [
-            {
-              $project: {
-                _id: 1,
-              },
-            },
-          ],
-        },
-      },
-      {
-        $unwind: "$user_portfolios", // 배열이 아닌 포트폴리오마다 문서를 생성
-      },
-      {
-        $lookup: {
-          // 각 유저 포트폴리오와 like join
-          from: "likes",
-          localField: "user_portfolios._id",
-          foreignField: "portfolioID",
-          as: "post_likes",
-          pipeline: [
-            {
-              $project: {
-                _id: 1,
-              },
-            },
-          ],
-        },
-      },
-      {
-        $addFields: {
-          // 포스트의 좋아요 수 필드 추가
-          like_count: { $size: "$post_likes" },
-        },
-      },
-      {
-        $group: {
-          // userID로 그룹화, totalLikes = 총 좋아요 수
-          _id: "$userID",
-          userID: { $first: "$userID" },
-          name: { $first: "$name" },
-          jobGroup: { $first: "$jobGroup" },
-          profileImage: { $first: "$profileImage" },
-          intro: { $first: "$intro" },
-          techStack: { $first: "$techStack" },
-          totalLikes: { $sum: "$like_count" },
-        },
-      },
-      {
-        $lookup: {
-          from: "techstacks",
-          localField: "techStack",
-          foreignField: "skill",
-          as: "techStack",
-          pipeline: [
-            {
-              $project: {
-                __v: 0,
-                _id: 0,
-              },
-            },
-          ],
-        },
-      },
-      {
-        $lookup: {
-          from: "jobgroups",
-          localField: "jobGroup",
-          foreignField: "_id",
-          as: "jobGroup",
-          pipeline: [
-            {
-              $project: {
-                __v: 0,
-                _id: 0,
-              },
-            },
-          ],
-        },
-      },
-      {
-        $unwind: "$jobGroup",
-      },
-      {
-        $addFields: {
-          jobGroup: "$jobGroup.job",
-        },
-      },
-      {
-        $project: {
-          _id: 0,
-        },
-      },
+      ...userProfilePipeline,
       {
         $sort: { totalLikes: -1 }, // 총 좋아요 수를 기준으로 내림차순 정렬
       },
